68635383145697f970e5f8447a555e4c21ea58c5
TechCrazi <smit@techcrazi.me>
2020-09-09 11:37:54 -0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-sudo apt-get install -y \
+sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg-agent \
     software-properties-common
 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 sudo apt-get update
-sudo apt-get -y install docker-engine docker-compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # add current user to docker group so there is no need to use sudo when running docker
+sudo groupadd docker
 sudo usermod -aG docker $(whoami)
