f7c5900f6b81f2f230f63b3fd2d44360ac5efd90
Smit Rabadia <smit@techcrazi.me>
2022-05-26 07:55:57 -0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -26,3 +26,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 # add current user to docker group so there is no need to use sudo when running docker
 sudo groupadd docker
 sudo usermod -aG docker $(whoami)
+
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
