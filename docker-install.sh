6cef16f26f8ec372bb8f8901ed6dc0bd053e4555
Smit Rabadia <smit@techcrazi.me>
2023-01-24 17:19:54 -0600

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -5,7 +5,7 @@ sudo apt-get install curl
 sudo apt-get install gnupg
 sudo apt-get install ca-certificates
 sudo apt-get install lsb-release
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt-get update
 
 sudo apt-get install \
@@ -35,3 +35,6 @@ sudo usermod -aG docker $(whoami)
 sudo curl -L https://github.com/docker/compose/releases/download/v2.14.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+sudo apt-get upgrade -y
+sudo apt-get update
