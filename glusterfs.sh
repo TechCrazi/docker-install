cdd21c5621f54c0617ffde457c666d930a31f065
Smit Rabadia <smit@techcrazi.me>
2023-02-04 12:24:24 -0600

    Update glusterfs.sh

--- glusterfs.sh
+++ glusterfs.sh
@@ -1,1 +1,7 @@
-
+#!/bin/bash
+sudo apt install fuse -y
+sudo apt install glusterfs-server -y
+sudo apt install glusterfs-client -y
+sudo systemctl enable --now glusterd
+sudo systemctl status glusterd
+sudo systemctl enable glusterd
